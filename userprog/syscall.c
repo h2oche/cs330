b69fc20dfebd0342ec866e843c8b07d566ace400
mgkim9704 <mgkim9704@kaist.ac.kr>
2018-10-09 20:04:00 +0900

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,17 +3,17 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+
 #include "threads/vaddr.h"
-#include "list.h"
-#include "process.h"
-#include "devices/input.h"
 #include "threads/malloc.h"
-#include "threads/init.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
 #include "threads/synch.h"
+#include "threads/init.h"
+#include "devices/input.h"
+#include <string.h>
 
-static struct semaphore sema_filesys;
+struct semaphore filesys_sema;
 static void syscall_handler (struct intr_frame *);
 
 /*---------------------------------------------------------------------------------------*/
@@ -23,36 +23,69 @@ Returns the byte value if successful, -1 if a segfault
 occurred. */
 static int get_user (const uint8_t *uaddr)
 {
+  if(!is_user_vaddr(uaddr))
+    return -1;
+
   int result;
   asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
   return result;
 }
 
 /*---------------------------------------------------------------------------------------*/
-//
-///* Writes BYTE to user address UDST.
-//UDST must be below PHYS_BASE.
-//Returns true if successful, false if a segfault occurred. */
-//static bool put_user (uint8_t *udst, uint8_t byte)
-//{
-//  int error_code;
-//  asm ("movl $1f, %0; movb %b2, %1; 1:" : "=&a" (error_code), "=m" (*udst) : "r" (byte));
-//  return error_code != -1;
-//}
-//
+
+/* user 포인터 확인하는 함수 */
+static bool is_valid_ptr(const void *base, unsigned size)
+{
+  uint8_t *ptr = (uint8_t *)base;
+  unsigned i = 0;
+
+  for(;i<size;i++){
+    if(get_user(ptr) == -1)
+      return false;
+    ptr++;
+  }
+  return true;
+}
+
 /*---------------------------------------------------------------------------------------*/
 
-/* fd를 file 포인터로 바꿔줌. 존재하지 않을 경우 NULL return */
-static struct file * fd_to_file (int fd)
+/* 스트링 끝날 때 까지('\0') 유효한 지 확인 */
+static bool is_valid_string(const void *str)
+{
+  int c;
+  uint8_t *ptr = (uint8_t *)str;
+
+  while((c=get_user(ptr)) != -1){
+    if(c=='\0')
+      return true;
+    ptr++;
+  }
+  return false;
+}
+
+/*---------------------------------------------------------------------------------------*/
+
+/* 버퍼 끝까지 유효한 지 확인 */
+static bool is_valid_buffer(const void *buffer, unsigned size)
+{
+  if(!is_valid_ptr(buffer, size))
+    return false;
+  return true;
+}
+
+/*---------------------------------------------------------------------------------------*/
+
+/* fd로 fd_info를 찾아줌. 존재하지 않을 경우 NULL return */
+static struct fd_info * get_fd_info (int fd)
 {
   struct thread* curr = thread_current();
   struct list_elem* le;
   struct fd_info* pfd_info;
-  
+
   for(le = list_begin(&curr->fd_infos); le != list_end(&curr->fd_infos); le = list_next(le)){
     pfd_info = list_entry(le, struct fd_info, elem);
     if(pfd_info->fd == fd)
-      return pfd_info->file;
+      return pfd_info;
   }
   return NULL;
 }
@@ -68,44 +101,9 @@ static void error_exit (void)
 
 /*---------------------------------------------------------------------------------------*/
 
-/* user 포인터 확인하는 함수 */
-static bool is_valid_ptr(const void *base, unsigned size)
-{
-  uint8_t *ptr = (uint8_t *)base;
-
-  /* get user 했을 때, segfault 발생하면 -1 return 됨 */
-  if(!is_user_vaddr(ptr))
-    return false;
-  else if(get_user(ptr+size-1) == -1)
-    return false; 
-  return true;
-}
-
-static bool is_valid_string(const char *str)
-{
-  int c;
-  uint8_t *p = (uint8_t *)str;
-  for(c=get_user(p); c != -1; c=get_user(p)){
-    if(c=='\0')
-      return true; // string 끝날 때 까지 다 valid 한 경우
-    p = p+1;
-  }
-  return false; // get_user 결과가 -1인 경우
-}
-
-static bool is_valid_buffer(const void * buffer, unsigned size)
-{
-  if(!is_valid_ptr(buffer, size))
-    return false;
-  return true;
-}
-
-/*---------------------------------------------------------------------------------------*/
-
 static void syscall_exit(struct intr_frame *f)
 {
-//  printf("syscall - exit\n");
-  if(!is_valid_ptr(f->esp+4, 4))
+  if(!is_valid_ptr(f->esp+4, sizeof(int)))
     return error_exit();
 
   int status = *(int *)(f->esp+4);
@@ -117,7 +115,6 @@ static void syscall_exit(struct intr_frame *f)
 
 static void syscall_exec(struct intr_frame *f)
 {
-//  printf("syscall - exec\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
@@ -125,6 +122,23 @@ static void syscall_exec(struct intr_frame *f)
   if(!is_valid_string(cmd_line))
     return error_exit();
 
+  /* TODO 파일 이름 파싱 -> 유효한 파일인 지 확인  */
+  char *save_ptr;
+  char *f_name = malloc(strlen(cmd_line)+1);
+  strlcpy (f_name, cmd_line, strlen(cmd_line)+1);
+  f_name = strtok_r (f_name, " ", &save_ptr);
+
+  sema_down(&filesys_sema);
+  struct file* file = filesys_open(f_name);
+  sema_up(&filesys_sema);
+
+  free(f_name);
+
+  if(file == NULL){
+    f->eax = -1;
+    return;
+  }
+    
   f->eax = process_execute(cmd_line);
 }
 
@@ -132,7 +146,6 @@ static void syscall_exec(struct intr_frame *f)
 
 static void syscall_wait(struct intr_frame *f)
 {
-//  printf("syscall - wait\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
@@ -144,7 +157,6 @@ static void syscall_wait(struct intr_frame *f)
 
 static void syscall_create(struct intr_frame *f)
 {
-//  printf("syscall - create\n");
   if(!is_valid_ptr(f->esp+4, 4) || !is_valid_ptr(f->esp+8, 4))
     return error_exit();
 
@@ -153,16 +165,15 @@ static void syscall_create(struct intr_frame *f)
     return error_exit();
   unsigned initial_size = *(unsigned *)(f->esp+8);
 
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
   f->eax = filesys_create(file, initial_size);
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_remove(struct intr_frame *f)
 {
-//  printf("syscall - create\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
@@ -170,16 +181,15 @@ static void syscall_remove(struct intr_frame *f)
   if(!is_valid_string(file))
     return error_exit();
 
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
   f->eax = filesys_remove(file);
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_open(struct intr_frame *f)
 {
-//  printf("syscall - open\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
@@ -189,13 +199,14 @@ static void syscall_open(struct intr_frame *f)
 
   int fd = -1;
 
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
   struct file* open_file = filesys_open(file);
 
-  /* process가 열 수 있는 파일 개수 128개로 제한 - FAQ */
-  if(file == NULL || thread_current()->next_fd > 128){
-    sema_up(&sema_filesys);
-    return error_exit();
+  /* process가 열 수 있는 파일 개수 제한 - FAQ */
+  if(open_file == NULL || thread_current()->next_fd > 130){
+    f->eax = fd;
+    sema_up(&filesys_sema);
+    return;
   }
 
   struct fd_info* pfd_info = malloc(sizeof(struct fd_info));
@@ -206,38 +217,40 @@ static void syscall_open(struct intr_frame *f)
   fd = pfd_info->fd;
   f->eax = fd;
 
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_filesize(struct intr_frame *f)
 {
-//  printf("syscall - filesize\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
   int fd = *(int *)(f->esp+4);
 
   int len = -1;
-  struct file* file = fd_to_file(fd);
+  struct fd_info* fd_info = get_fd_info(fd);
+  if(fd_info == NULL)
+    return error_exit(); 
+
+  struct file* file = fd_info->file;
 
   if(file == NULL){
     return error_exit();
   }
 
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
 
   len = file_length(file);
   f->eax = len;
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_read(struct intr_frame *f)
 {
-//  printf("syscall - read\n");
   if(!is_valid_ptr(f->esp+4, 4) || !is_valid_ptr(f->esp+8,4) || !is_valid_ptr(f->esp+12, 4))
     return error_exit();
 
@@ -254,29 +267,38 @@ static void syscall_read(struct intr_frame *f)
   /* stdin 인 경우 */
   if(fd == 0){
     uint8_t* stdin_buf = (uint8_t *)buffer;
+    sema_down(&filesys_sema);
     for(i=0; i<size; i++){
       stdin_buf[i] = input_getc();
     }
     read_size = size;
+    sema_up(&filesys_sema);
+  }
+  else if(fd == 1){ /* stdout */
+    return error_exit();
   }
   else{
-    struct file* file = fd_to_file(fd);
+    struct fd_info* fd_info = get_fd_info(fd);
+    if(fd_info == NULL)
+      return error_exit();
+
+    struct file* file = fd_info->file;
     if(file == NULL){
       return error_exit();
     }
-    sema_down(&sema_filesys);
+    sema_down(&filesys_sema);
     read_size = file_read(file, buffer, size);
-    sema_up(&sema_filesys);
+    sema_up(&filesys_sema);
   }
 
   f->eax = read_size;
 }
 
+
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_write(struct intr_frame *f)
 {
-//  printf("syscall - write\n");
   if(!is_valid_ptr(f->esp+4, 4) || !is_valid_ptr(f->esp+8, 4) || !is_valid_ptr(f->esp+12, 4))
     return error_exit();
 
@@ -284,30 +306,28 @@ static void syscall_write(struct intr_frame *f)
   const void *buffer = *(char **)(f->esp+8);
   unsigned size = *(unsigned *)(f->esp+12);
 
-//  printf("esp: %p\n", f->esp);
-//  hex_dump(PHYS_BASE - 0x90, PHYS_BASE - 0x90, 0x90, true);
-//  printf("buffer address: %p\n", &buffer);
-//  printf("pointer: %p \n", buffer);
-
   if(!is_valid_buffer(buffer, size))
     return error_exit();
 
   int write_size = -1;
   /* stdout 인 경우 */
   if(fd == 1){
-    sema_down(&sema_filesys);
+    sema_down(&filesys_sema);
     putbuf((char *)buffer, (size_t)size);
     write_size = (int)size;
-    sema_up(&sema_filesys);
+    sema_up(&filesys_sema);
   }
   else{
-    struct file* file = fd_to_file(fd);
+    struct fd_info *fd_info = get_fd_info(fd);
+    if(fd_info == NULL)
+      return error_exit();
+    struct file* file = fd_info->file;
     if(file == NULL){
       return error_exit();
     }
-    sema_down(&sema_filesys);
+    sema_down(&filesys_sema);
     write_size = file_write(file, buffer, size);
-    sema_up(&sema_filesys);
+    sema_up(&filesys_sema);
   }
 
   f->eax = write_size;
@@ -317,77 +337,87 @@ static void syscall_write(struct intr_frame *f)
 
 static void syscall_seek(struct intr_frame *f)
 {
-//  printf("syscall - seek\n");
   if(!is_valid_ptr(f->esp+4, 4) || !is_valid_ptr(f->esp+8, 4))
     return error_exit();
 
   int fd = *(int *)(f->esp+4);
   unsigned position = *(unsigned *)(f->esp+8);
 
-  struct file* file = fd_to_file(fd);
+  struct fd_info* fd_info = get_fd_info(fd);
+  if(fd_info == NULL)
+    return error_exit();
+  struct file* file = fd_info->file;
   if(file == NULL){
     return error_exit();
   }
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
   file_seek(file, position);
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_tell(struct intr_frame *f)
 {
-//  printf("syscall - tell\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
   int fd = *(int *)(f->esp+4);
 
-  struct file* file = fd_to_file(fd);
+  struct fd_info* fd_info = get_fd_info(fd);
+  if(fd_info == NULL)
+    return error_exit();
+  struct file* file = fd_info->file;
   if(file == NULL){
     return error_exit();
   }
-  sema_down(&sema_filesys);
+  sema_down(&filesys_sema);
   f->eax = file_tell(file);
-  sema_up(&sema_filesys);
+  sema_up(&filesys_sema);
 }
 
 /*---------------------------------------------------------------------------------------*/
 
 static void syscall_close(struct intr_frame *f)
 {
-//  printf("syscall - close\n");
   if(!is_valid_ptr(f->esp+4, 4))
     return error_exit();
 
   int fd = *(int *)(f->esp+4);
 
-  struct file* file = fd_to_file(fd);
-  if(file == NULL){
+  /* stdin stdout 닫는 경우 */
+  if(fd==0 || fd==1)
     return error_exit();
-  }
-  sema_down(&sema_filesys);
+
+  struct fd_info *fd_info = get_fd_info(fd);
+  if(fd_info == NULL)
+    return error_exit();
+
+  struct file* file = fd_info->file;
+  if(file == NULL)
+    return error_exit();
+  
+  sema_down(&filesys_sema);
   file_close(file);
-  sema_up(&sema_filesys);
+  fd_info->file = NULL;
+  sema_up(&filesys_sema);
 }
+
 
 /*---------------------------------------------------------------------------------------*/
 
-void syscall_init (void) 
+void
+syscall_init (void) 
 {
+  sema_init(&filesys_sema, 1);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-  sema_init (&sema_filesys, 1);
 }
 
-/*---------------------------------------------------------------------------------------*/
-
-static void syscall_handler (struct intr_frame *f UNUSED) 
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-
-  if(!is_valid_ptr(f->esp, 4)){
+  if(!is_valid_ptr(f->esp, 4))
     return error_exit();
-  }
 
   int syscall_number = *(int *)f->esp;
   switch(syscall_number){
@@ -434,5 +464,4 @@ static void syscall_handler (struct intr_frame *f UNUSED)
       return error_exit();
   }
 
-  thread_exit ();
 }
