24917252005769fd561dfd42576476b9924655b4
mgkim9704 <mgkim9704@kaist.ac.kr>
2018-10-09 20:04:19 +0900

    Update syscall.h

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,15 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-//
-//struct fd_info
-//{
-//  int fd;
-//  struct file* file;
-//  struct list_elem elem;
-//};
-//
-//
-void syscall_init (void);
 
+#include "userprog/process.h"
+
+void syscall_init (void);
 
 #endif /* userprog/syscall.h */
